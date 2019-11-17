a8126e665900bb4d2565525cabb87083657f113f
Benjamin Wofford <woffordben1992@gmail.com>
2019-11-16 19:12:14 -0500

    Added code to the bubble sort method. Debugging needed.

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,16 +1,31 @@
 #bubble sort this array
 
-[3, 6, 4, 8, 2, 9, 6]
+require "byebug"
 
-def bubble_sort
+def bubble_sort(array)
+    sorted = false
 
-    #input array
+    debugger
 
+   while sorted == false
 
+        (0...array.length - 1).each do |number|
+            sorted = true
 
+            if array[number] < array[number + 1]
+            array[number], array[number + 1] = array[number + 1], array[number]
+            sorted = false
+            end
 
+        end
+    end
+
+    return array
 
 end
 
+array_to_sort = [3, 6, 4, 8, 2, 9, 6]
 
+bubble_sorted_array = bubble_sort(array_to_sort)
+p bubble_sorted_array
 # the output should look like [2, 3, 4, 6, 6, 8, 9]
